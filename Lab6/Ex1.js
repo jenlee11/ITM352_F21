bcc81b8262c15447deb2de820cbf7d6dda26c52c
jenlee11 <yejinj@hawaii.edu>
2021-09-21 23:25:41 -1000

    9/21work

---
+++ Lab6/Ex1.js
@@ -0,0 +1,8 @@
+var age =  31;
+var fav_num = 2;
+var day_of_birth = 7;
+var month_of_birth = 5;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
