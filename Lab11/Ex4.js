901594971695960ee18c0e35bdcf66565f15f506
jenlee11 <yejinj@hawaii.edu>
2021-11-02 10:12:04 -1000

    2

--- Lab11/Ex4.js
+++ Lab11/Ex4.js
@@ -1,15 +1,10 @@
-
-function isNonNegInt(q, callback = console.log) {
-    // q is a callback fun
-    errors = []; //assume no errors at first. If returnErrors is true, the array of errors is returned,
-    //others returns true if q is a non-neg int.
+function isNonNegInt(val, returnErrors=false) {
     errors = []; //assume no errors at first 
-    if (Number(q) !=q) errors.push('Not a number!'); //check if string is a number value
-    if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
-    if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-    
-    callback(errors);
-    // return returnErrors ? errors : (errors.length == 0)
+ if (Number(q) !=q) errors.push('Not a number!'); //check if string is a number value
+ if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
+ if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+   
+ return returnErrors ? errors : ((errors.length > 0) ? false:true);
 }
 isNonNegInt("-1.34");
 isNonNegInt("-1");
--- WODs/Inv4/products_data.js
+++ WODs/Inv4/products_data.js
@@ -1,31 +1,31 @@
 
 var products = [
 
- { brand : "HTC",
+ { name : "HTC",
    price : 40.00,
   image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg" },
 
 
 
- { brand : "Apple",
+ { name : "Apple",
    price : 75.00,
   image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg" },
 
 
 
- { brand : "Nokia",
+ { name : "Nokia",
    price : 35.00,
   image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg" },
 
 
 
- { brand : "Samsung",
+ { name : "Samsung",
   price : 45.00,
   image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg" },
 
 
 
- { brand : "Blackberry",
+ { name : "Blackberry",
   price : 10.00,
   image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg" }
 
