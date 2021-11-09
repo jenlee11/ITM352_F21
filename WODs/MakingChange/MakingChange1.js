23ca177b479793d04e0ee96824b56004c4fbc85f
jenlee11 <yejinj@hawaii.edu>
2021-11-09 00:08:50 -1000

    1

---
+++ WODs/MakingChange/MakingChange1.js
@@ -0,0 +1,26 @@
+var coins_input = quarters, dimes, nickels, pennies;
+
+quarters = dimes = nickels = pennies = 0;
+coins_input = 73;
+
+// Get the max number of quarters
+
+// Get the max number of dimes from leftover amount
+
+// Get the max number of nickles from leftvoer amount
+
+// What's left will be 0-4 pennies
+
+print("%2d quarters\n%2d dimes\n%2d nickels\n%2d pennies\n", quarters, dimes, nickels, pennies);
+
+Quarters = int (amount%25);
+leftover = amount%25;
+Dimes = int(leftover/10);
+leftover = amount%10;
+nickels = int(leftover/5);
+leftover = amount%5;
+Pennies = amount%5;
+
+int (num = amount%25);
+}
+console.log(`${Quarters, Dimes, Nickles, Pennies}`);
